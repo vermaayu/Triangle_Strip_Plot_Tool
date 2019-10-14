2e5947d52bbe173bf50b086901640f81275ef427
Aayush Verma <vermaayu@gmail.com>
2019-10-14 16:42:50 +0200

    Add files via upload

--- TriangleStripPlotTool.cpp
+++ TriangleStripPlotTool.cpp
@@ -8,8 +8,8 @@
 #include <math.h>
 #include <fstream>
 
-#define W_WIDTH  500
-#define W_HEIGHT  500
+#define W_WIDTH  1000
+#define W_HEIGHT  1000
 
 using namespace std;
 
@@ -35,40 +35,43 @@ public:
 /////////////////////GLOBAL VARIABLES////////////////////////////////////////////
 
 float h1,h2;
-Point arr[10];
+Point arr[100];
 int index = 0;
 int flag = 0;
 
 /////////////////////////////////////////////////////////////////////////////////
 
-void drawDot(int x, int y) {
-	glClear(GL_COLOR_BUFFER_BIT); 
-	glColor3f(0.0,1.0,0.0);
+void drawDot(float x, float y) {
+	//glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f(1.0,1.0,0.0);
 	glPointSize(10.0);
 	glBegin(GL_POINTS);
 	glVertex2f(x,y);
 	glEnd();
+	glFlush();
 }
 
 
 void drawLine(float x1 , float y1, float x2, float y2){
-	glClear(GL_COLOR_BUFFER_BIT); 
-	glColor3f(0.0,0.0,1.0);
+	//glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f(1.0,0.0,0.0);
 	glBegin(GL_LINES);
     glVertex2f(x1,y1);
     glVertex2f(x2,y2);
 	glEnd();
+	glFlush();
 }
 
 
 void drawStrip(float x1 , float y1, float x2, float y2, float x3, float y3){
-	glClear(GL_COLOR_BUFFER_BIT); 
-	glColor3f(0.0,0.0,1.0);
+	//glClear(GL_COLOR_BUFFER_BIT); 
+	glColor3f(0.0,1.0,0.0);
 	glBegin(GL_TRIANGLE_STRIP);
     glVertex2f(x1,y1);
     glVertex2f(x2,y2);
 	glVertex2f(x3,y3);
 	glEnd();
+	glFlush();
 }
 
 
@@ -79,53 +82,67 @@ void drawStrip(float x1 , float y1, float x2, float y2, float x3, float y3){
 
 void mouse(int button, int state , int x , int y) {
 if(button == GLUT_LEFT_BUTTON && state == GLUT_UP) 
-{	cout << "(" << (x) << ", " << (500-y) << ")\n" << endl;			// Figures will be made in the first quadrant.
-	arr[index].setxy((float)x,(float)(500 - y));
+{	cout << "(" << (x-(W_WIDTH/2)) << ", " << ((W_HEIGHT/2)-y) << ")\n" << endl;			// Figures will be made in the first quadrant.
+	arr[index].setxy((float)(x-(W_WIDTH/2)),(float)((W_HEIGHT/2)-y));
     index++;
-	drawDot(x,y);
+	
 }
 	
 else if(button == GLUT_RIGHT_BUTTON && state == GLUT_UP) 
 {
 	ofstream myfile;
 	myfile.open ("Coordinate.txt");
-	for(int i = 0 ;i<(sizeof(arr)/sizeof(*arr)); i++){
+	for(int i = 0 ;i<(sizeof(arr)/sizeof(*arr)); i++)
+	{
 		myfile << "(" << arr[i].x << ", " << arr[i].y << ")\n";
 	}
 	myfile.close();
+}
+
+else if(button == GLUT_MIDDLE_BUTTON && state == GLUT_UP)
+{
+
 
 }
 
-	if(flag == 3)
-	{
-		drawStrip(arr[index-2].x , arr[index-2].y, arr[index-1].x, arr[index-1].y, arr[index].x, arr[index].y);
-	}
-	else
-	{
-		flag++;
-	}
-
-
-	glutPostRedisplay();					//////////////////////////////Check this
+	
+glutPostRedisplay();					//////////////////////////////Check this
 }
 
 /*--------------Window Creation Specs--------------------------------------*/
-void init()
-{
-	glClearColor(0.0,1.0,1.0,0.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
-}
 
 
 void display()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
- 
 	
- 
-   glFlush(); 
+
+
+	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set background color to black and opaque
+	//glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
+	//glutMouseFunc(mouse);
+
+	drawDot(arr[index-1].x, arr[index-1].y);
+	
+	if(flag >= 3)
+	{
+		drawStrip(arr[index-3].x , arr[index-3].y, arr[index-2].x, arr[index-2].y, arr[index-1].x, arr[index-1].y);
+
+		//cout<< "\n "<<arr[index-2].x << " "<< arr[index-2].y << " - "<< arr[index-1].x << " " << arr[index-1].y << " - " << arr[index].x << " " << arr[index].y <<endl;
+		
+		//cout<< arr[index].x << " " << arr[index].y <<endl;
+
+
+		drawLine(arr[index-2].x, arr[index-2].y, arr[index-1].x, arr[index-1].y);
+	}
+
+	else
+	{
+		drawLine(arr[index-1].x, arr[index-1].y, arr[index].x, arr[index].y);
+		flag++;
+
+	}
+
+	glFlush(); 
 }
 /*--------------Window Creation Specs Ends--------------------------------------*/
 
@@ -135,14 +152,18 @@ void display()
 int main(int argc,char **argv)
 {
 	
-	glutInit(&argc,argv);		
+	glutInit(&argc,argv);	
 	
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
 	glutInitWindowPosition(2000,200);
 	glutCreateWindow("Triangle_Strip_Workspace");
+	gluOrtho2D(-(W_WIDTH/2), W_WIDTH/2,-(W_HEIGHT/2),W_HEIGHT/2);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
+	
+	
 	
 	glutMainLoop();
 	
